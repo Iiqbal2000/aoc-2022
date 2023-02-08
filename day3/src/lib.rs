54e5b04f78ea65cc9d9e5665acf39c66f590bccd
Iqbal H <52209787+Iiqbal2000@users.noreply.github.com>
2023-02-08 23:23:55 +0000

    done day 3

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -1,3 +1,5 @@
+use std::io::{Read, BufReader};
+
 fn detect_compartment(rucksack: &str) -> (&str, &str) {
   let median = rucksack.len() / 2;
   let (first, last) = rucksack.split_at(median);
@@ -11,6 +13,33 @@ fn common_char(x: &str, y: &str) -> char {
     if y.contains(c) {
       result = c;
     }
+  }
+
+  result
+}
+
+fn find_common_char_multiple(items: &Vec<&str>) -> char {
+  let mut result: char = 'a';
+
+  let mut base_item: Vec<char> = items[0].chars().collect();
+
+  for bi in &base_item {
+    let mut found_in_all_lines = false;
+
+    for item in items[1..].iter() {
+      if !item.contains(*bi) {
+        found_in_all_lines = false;
+        break;
+      }
+
+      found_in_all_lines = true
+    }
+
+    if found_in_all_lines {
+      result = *bi;
+      break;
+    }
+    
   }
 
   result
@@ -49,6 +78,7 @@ fn find_priority(c: char) -> usize {
   }
 }
 
+// Part 1
 pub fn exec(input: String) -> usize {
   let result:usize = input.lines().map(|c| {
     let (l,f) = detect_compartment(c);
@@ -57,6 +87,23 @@ pub fn exec(input: String) -> usize {
   }).sum();
 
   result
+}
+
+// Part 2
+pub fn exec_multiple(input: String) -> usize {
+  let mut buf: Vec<&str> = Vec::new();
+  let mut priorities: Vec<usize> = Vec::new();
+
+  for line in input.lines() {
+    buf.push(line);
+    if buf.len() == 3 {
+      let common_c = find_common_char_multiple(&buf);
+      priorities.push(find_priority(common_c));
+      buf.clear();
+    }
+  }
+
+  priorities.iter().sum()
 }
 
 #[cfg(test)]
@@ -98,5 +145,27 @@ mod tests {
     assert_eq!(16, find_priority('p'));
     assert_eq!(38, find_priority('L'));
     assert_eq!(42, find_priority('P'));
+  }
+
+  #[test]
+  fn find_common_char_in_multiple_lines() {
+    let items = vec![
+        "vJrwpWtwJgWrhcsFMMfFFhFp",
+        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+        "PmmdzqPrVvPwwTWBwg",
+    ];
+
+    let result = find_common_char_multiple(&items);
+    assert_eq!('r', result);
+
+    let items = vec![
+      "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+      "ttgJtRGJQctTZtZT",
+      "CrZsJsPPZsGzwwsLwLmpwMDw",
+  ];
+
+  let result = find_common_char_multiple(&items);
+  assert_eq!('Z', result);
+    
   }
 }
--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,12 +1,12 @@
 use std::io::{self, Read};
 
-use day3::exec;
+use day3::*;
 
 fn main() {
     let mut buffer = String::new();
     let mut stdin = io::stdin();
     stdin.read_to_string(&mut buffer);
 
-    let total = exec(buffer);
+    let total = exec_multiple(buffer);
     println!("{total}");
 }
